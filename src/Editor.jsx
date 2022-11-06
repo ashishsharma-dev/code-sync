0dd32c683c8dccf72ffd4b74742cfca613e53c48
Ashish Sharma <sharmaashish049@gmail.com>
2022-11-06 16:07:32 +0000

    Update 5 files

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect } from "react";
 import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
 import "codemirror/theme/dracula.css";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
 
 const Editor = () => {
   useEffect(() => {
     const init = async () => {
+      console.log("hellow");
       Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
         mode: { name: "javascript", json: true },
         theme: "dracula",
         autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
       });
     };
 
     init();
-  }, []);
+  });
   return (
     <div>
-      <textarea placeholder="//Code goes here" id="realtimeEditor" />
+      <textarea id="realtimeEditor" />
     </div>
   );
 };
